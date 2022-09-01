a87b91be7212e701133e9c7f620bf067866b1579
Marwa <marwahakmi@gmail.com>
2022-09-01 11:21:20 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const input = document.querySelector('#new-task-description')
+    console.log(input.value)
+    addToDo(input.value)
+  })
 });
+
+const addToDo = (todo) => {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+
+  p.textContent = `${todo} `;
+  btn.textContent = " x "
+
+  document.querySelector('#tasks').appendChild(p);
+  p.appendChild(btn);
+
+  btn.addEventListener('click', (event) => deleteToDo(event))
+}
+
+const deleteToDo = (event) => {
+  event.target.parentNode.remove();
+}
